68f56e0dc396cddcdff2d7522ebe7c102cc0bfd2
Scotty Simpson <portrayme@mac.com>
2017-07-07 14:25:51 -0700

    Create picker.js

---
+++ picker.js
@@ -0,0 +1,158 @@
+require('./style.sass')
+
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+var longMonths = [0, 2, 4, 6, 7, 9, 11]
+
+function adjustedDateObj(month, year, delta) {
+	month += delta
+
+	var over = month > 11
+	var under = month < 0
+
+	return {
+		month: over ? 0 : under ? 11 : month,
+		year: over ? year + 1 : under ? year - 1 : year
+	}
+}
+
+function lastDateInMonth(month, year, delta) {
+	var obj = adjustedDateObj(month, year, delta)
+	if (longMonths.indexOf(obj.month) > -1) return 31
+	if (obj.month === 1) return (obj.year % 4 > 0) ? 28 : 29
+	return 30
+}
+
+function daysFromLastMonth(dateObj){
+	var month = dateObj.month, year = dateObj.year
+	var newDate = new Date(year, month, 1)
+	var day = newDate.getDay()
+	var array = []
+	if (day > 0) {
+		var len = lastDateInMonth(month, year, -1)
+		for (var i=len-day+1; i<=len; i++) { array.push(i) }
+	}
+	return array
+}
+
+function daysFromThisMonth(dateObj) {
+	var max = lastDateInMonth(dateObj.month, dateObj.year, 0)
+	var array = []
+	for (var i=1; i<=max; i++) {
+		array.push(i)
+	}
+	return array
+}
+
+function daysFromNextMonth(dateObj) {
+	var month = dateObj.month, year = dateObj.year
+	var lastDate = lastDateInMonth(month, year, 0)
+	var newDate = new Date(year, month, lastDate)
+	var day = newDate.getDay()
+	var array = []
+	if (day < 6) {
+		for (var i=1; i<=6-day; i++) { array.push(i) }
+	}
+	return array
+}
+
+function setMonth(vnode, delta) {
+	var obj = adjustedDateObj(vnode.state.month, vnode.state.year, delta)
+	vnode.state.month = obj.month
+	vnode.state.year = obj.year
+}
+
+function displayDate(date) {
+	return days[date.getDay()] + ', ' + months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+}
+
+function classForDateBox(vnode, date) {
+	if (vnode.state.year !== vnode.state.date.getFullYear() || 
+		vnode.state.month !== vnode.state.date.getMonth()) return ''
+	
+	return (vnode.state.date.getDate() === date) ? 'chosen' : ''
+}
+
+module.exports = {
+	active: false,
+	oninit: function (vnode) {
+		vnode.state.date = vnode.attrs.date || new Date()
+		vnode.state.month = vnode.state.date.getMonth()
+		vnode.state.year = vnode.state.date.getFullYear()
+	},
+	view: function (vnode) {
+		var dateObj = {
+			month: vnode.state.month,
+			year: vnode.state.year
+		}
+		return m('.crewmonkey'
+			, m('.date-picker'
+				, m('.current-date'
+					, {
+						onclick: function () {
+							vnode.state.active = !vnode.state.active
+						}
+					}
+					, displayDate(vnode.state.date)
+				)
+				, vnode.state.active
+					? m('.calendar'
+						, m('.month-header'
+							, m('button'
+								, { onclick: setMonth.bind(null, vnode, -1) }
+								, '<'
+							)
+							, m('h2', months[dateObj.month] + ' ' + dateObj.year)
+							, m('button'
+								, { onclick: setMonth.bind(null, vnode, 1) }
+								, '>'
+							)
+						)
+						, m('.weekdays'
+							, days.map(function (day) {
+								return m('.day.dummy', day.substring(0, 1))
+							})
+						)
+						, m('.weekdays'
+							, {
+								onclick: function (e) {
+									var box = e.target
+									var date = parseInt(box.textContent)
+									if (box.classList.contains('not-this-month')) {
+
+										if (date > 6) { // 6 === max days to display from prev or next month
+											setMonth(vnode, -1)
+										}
+										else if (date < 6) {
+											setMonth(vnode, 1)
+										}
+									}
+									vnode.state.date.setYear(vnode.state.year)
+									vnode.state.date.setMonth(vnode.state.month)
+									vnode.state.date.setDate(date)
+
+									vnode.state.active = false
+									
+									if (vnode.attrs.commit) vnode.attrs.commit(vnode.state.date)
+								}
+							}
+							, daysFromLastMonth(dateObj).map(function (date) {
+								return m('.day.not-this-month', date)
+							})
+							, daysFromThisMonth(dateObj).map(function (date) {
+								return m('.day'
+									, { class: classForDateBox(vnode, date) }
+									, date
+								)
+							})
+							, daysFromNextMonth(dateObj).map(function (date) {
+								return m('.day.not-this-month', date)
+							})
+						)
+					)
+					: null
+			)
+		)
+	}
+}
