4bdca2f7b7d688d2a955684d31fe659923474fa7
Scotty Simpson <portrayme@mac.com>
2017-07-07 17:59:51 -0700

    Update picker.js

--- picker.js
+++ picker.js
@@ -1,4 +1,4 @@
-require('./style.sass')
+require('./style.css')
 
 var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
