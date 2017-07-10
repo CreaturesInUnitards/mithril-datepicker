3dd333ca66ae6eba37f73883d7e0b0c70b97da51
scotty <portrayme@mac.com>
2017-07-10 08:14:01 -0700

    mild refactor: moved to global State added YearView broke into sub-components fixed backing-into-February bug

--- mithril-datepicker.js
+++ mithril-datepicker.js
@@ -1,194 +1,240 @@
-if (typeof window.require === 'function') require('./style.sass')
+;(function (window) {
+	if (typeof window.require === 'function') require('./style.sass')
 
-var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-var longMonths = [0, 2, 4, 6, 7, 9, 11]
-
-function adjustedDateObj(dateObj, delta) {
-	var month = dateObj.month, year = dateObj.year
-	month += delta
-
-	var over = month > 11
-	var under = month < 0
-
-	return {
-		month: over ? 0 : under ? 11 : month,
-		year: over ? year + 1 : under ? year - 1 : year
-	}
-}
-
-function lastDateInMonth(dateObj, delta) {
-	var obj = adjustedDateObj(dateObj, delta)
-	if (longMonths.indexOf(obj.month) > -1) return 31
-	if (obj.month === 1) {
-		if (!(obj.year % 400)) return 29
-		if (!(obj.year % 100)) return 28
-		return (obj.year % 4) ? 28 : 29
-	}
-
-	return 30
-}
-
-function daysFromLastMonth(dateObj){
-	var month = dateObj.month, year = dateObj.year
-	var day = (new Date(year, month, 1)).getDay()
-	var array = []
-	if (day > 0) {
-		var len = lastDateInMonth(dateObj, -1)
-		for (var i=len-day+1; i<=len; i++) { array.push(i) }
-	}
-	return array
-}
-
-function daysFromThisMonth(dateObj) {
-	var max = lastDateInMonth(dateObj, 0)
-	var array = []
-	for (var i=1; i<=max; i++) {
-		array.push(i)
-	}
-	return array
-}
-
-function daysFromNextMonth(dateObj) {
-	var month = dateObj.month, year = dateObj.year
-	var lastDate = lastDateInMonth(dateObj, 0)
-	var day = (new Date(year, month, lastDate)).getDay()
-	var array = []
-	if (day < 6) {
-		for (var i=1; i<=6-day; i++) { array.push(i) }
-	}
-	return array
-}
-
-function setMonth(state, delta) {
-	state.dateObj = adjustedDateObj(state.dateObj, delta) 
-}
-
-function displayDate(date) {
-	return days[date.getDay()].substring(0, 3) + ' ' + months[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear()
-}
-
-function classForDateBox(vnode, date) {
-	// TODO: if the chosen date is visible but in 'other' month, it should still get the 'chosen' class
-	var dateObj = vnode.state.dateObj
-	if (dateObj.year !== vnode.state.date.getFullYear() || 
-		dateObj.month !== vnode.state.date.getMonth()) return ''
+	var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	var longMonths = [0, 2, 4, 6, 7, 9, 11]
 	
-	return (vnode.state.date.getDate() === date) ? 'chosen' : ''
-}
+	var State = {
+		date: null,
+		viewObj: {},
+		active: false,
+		yearView: false,
+		chooseDate: function (vnode, e) {
+			var box = e.target
+			var date = parseInt(box.textContent)
 
-function defaultDate(){
-	var now = new Date()
-	now.setHours(0, 0, 0, 0)
-	return now
-}
+			if (box.classList.contains('not-this-month')) {
+				State.stepMonth(otherMonthIsPrev(date) ? -1 : 1)
+			}
 
-function chooseDate(vnode, e) {
-	var box = e.target
-	var date = parseInt(box.textContent)
-	
-	if (box.classList.contains('not-this-month')) {
+			State.date.setFullYear(State.viewObj.year, State.viewObj.month, date)
 
-		if (date > 6) { // 6 === max days to display from prev or next month
-			setMonth(vnode.state, -1)
-		}
-		else if (date <= 6) {
-			setMonth(vnode.state, 1)
+			if (vnode.attrs.commit) vnode.attrs.commit(State.date)
+
+			State.active = false
+		},
+		jumpToMonth: function (month) {
+			State.viewObj.month = month
+			State.yearView = false
+		},
+		stepMonth: function(delta) {
+			State.viewObj = adjustedViewObj(State.viewObj, delta)
+		},
+		stepYear: function (delta) {
+			State.viewObj.year += delta
 		}
 	}
-	vnode.state.date.setYear(vnode.state.dateObj.year)
-	vnode.state.date.setMonth(vnode.state.dateObj.month)
-	vnode.state.date.setDate(date)
 
-	vnode.state.active = false
-
-	if (vnode.attrs.commit) vnode.attrs.commit(vnode.state.date)
-}
-
-var DatePicker = {
-	active: false,
-	oninit: function (vnode) {
-		vnode.state.date = vnode.attrs.date || defaultDate()
-		vnode.state.dateObj = {
-			month: vnode.state.date.getMonth(),
-			year: vnode.state.date.getFullYear()
+	var fadeComponent = function(){
+		return {
+			oncreate: function (vnode) {
+				requestAnimationFrame(function () { vnode.dom.classList.remove('incoming') })
+			},
+			onbeforeremove: function (vnode) {
+				vnode.dom.classList.add('incoming')
+				return new Promise(function (done) {
+					setTimeout(done, 2000)
+				})
+			}
 		}
-	},
-	view: function (vnode) {
-		var dateObj = vnode.state.dateObj
-		return m('.container'
-			, m('.mithril-date-picker'
-				
-				// chosen/default date display
-				, m('button.current-date'
-					, {
-						onclick: function () {
-							vnode.state.active = !vnode.state.active
-							vnode.state.dateObj = {
-								month: vnode.state.date.getMonth(),
-								year: vnode.state.date.getFullYear()
-							}
-						}
-					}
-					, displayDate(vnode.state.date)
-				)
-				, vnode.state.active
-					
-					// calendar/picker
-					? m('.calendar.incoming'
+	}
+
+	function adjustedViewObj(viewObj, delta) {
+		var month = viewObj.month, year = viewObj.year
+		month += delta
+
+		var over = month > 11
+		var under = month < 0
+
+		return {
+			month: over ? 0 : under ? 11 : month,
+			year: over ? year + 1 : under ? year - 1 : year
+		}
+	}
+
+	function lastDateInMonth(viewObj, delta) {
+		var obj = adjustedViewObj(viewObj, delta)
+		if (longMonths.indexOf(obj.month) > -1) return 31
+		if (obj.month === 1) {
+			if (!(obj.year % 400)) return 29
+			if (!(obj.year % 100)) return 28
+			return (obj.year % 4) ? 28 : 29
+		}
+
+		return 30
+	}
+
+	function daysFromLastMonth(viewObj){
+		var month = viewObj.month, year = viewObj.year
+		var day = (new Date(year, month, 1)).getDay()
+		var array = []
+		if (day > 0) {
+			var len = lastDateInMonth(viewObj, -1)
+			for (var i=len-day+1; i<=len; i++) { array.push(i) }
+		}
+		return array
+	}
+
+	function daysFromThisMonth(viewObj) {
+		var max = lastDateInMonth(viewObj, 0)
+		var array = []
+		for (var i=1; i<=max; i++) {
+			array.push(i)
+		}
+		return array
+	}
+
+	function daysFromNextMonth(viewObj) {
+		var month = viewObj.month, year = viewObj.year
+		var lastDate = lastDateInMonth(viewObj, 0)
+		var day = (new Date(year, month, lastDate)).getDay()
+		var array = []
+		if (day < 6) {
+			for (var i=1; i<=6-day; i++) { array.push(i) }
+		}
+		return array
+	}
+
+	function displayDate(date) {
+		return days[date.getDay()].substring(0, 3) + ' ' + months[date.getMonth()] + ' ' + date.getDate() + ' ' + date.getFullYear()
+	}
+
+	function classForDateBox(date) {
+		// TODO: if the chosen date is visible but in 'other' month, it should still get the 'chosen' class
+		var viewObj = State.viewObj
+		if (viewObj.year !== State.date.getFullYear() ||
+			viewObj.month !== State.date.getMonth()) return ''
+		
+		return (State.date.getDate() === date) ? 'chosen' : ''
+	}
+
+	function classForMonthBox(month) {
+		return (State.date.getMonth() === month && State.date.getFullYear() === State.viewObj.year) ? 'chosen' : ''
+	}
+
+	function defaultDate(){
+		var now = new Date()
+		now.setHours(0, 0, 0, 0)
+		return now
+	}
+
+	function otherMonthIsPrev(n) {
+		return n > 6 // 6 === max days to display from prev or next month
+	}
+
+	var DatePicker = {
+		active: false,
+		oninit: function (vnode) {
+			State.date = vnode.attrs.date || defaultDate()
+			State.viewObj = {
+				month: State.date.getMonth(),
+				year: State.date.getFullYear()
+			}
+		},
+		view: function (vnode) {
+			var viewObj = State.viewObj
+			return m('.container'
+				, m('.mithril-date-picker'
+
+					// chosen/default date display
+					, m('button.current-date'
 						, {
-							oncreate: function (vnode) {
-								requestAnimationFrame(function () { vnode.dom.classList.remove('incoming') })
-							},
-							onbeforeremove: function (vnode) {
-								vnode.dom.classList.add('incoming')
-								return new Promise(function (done) {
-									setTimeout(done, 200)
-								})
+							onclick: function () {
+								State.active = !State.active
+								State.yearView = false
+								State.viewObj = {
+									month: State.date.getMonth(),
+									year: State.date.getFullYear()
+								}
 							}
 						}
-						
-						// header
-						, m('.month-header'
-							, m('button.prev'
-								, { onclick: setMonth.bind(null, vnode.state, -1) }
-							)
-							, m('button.month-year', months[dateObj.month] + ' ' + dateObj.year)
-							, m('button.next'
-								, { onclick: setMonth.bind(null, vnode.state, 1) }
-							)
-						)
-						
-						// weekday column labels
-						, m('.weekdays'
-							, days.map(function (day) {
-								return m('.day.dummy', day.substring(0, 1))
-							})
-						)
-						
-						// clickable day blocks
-						, m('.weekdays'
-							, { onclick: chooseDate.bind(null, vnode) }
-							, daysFromLastMonth(dateObj).map(function (date) {
-								return m('button.day.not-this-month', date)
-							})
-							, daysFromThisMonth(dateObj).map(function (date) {
-								return m('button.day'
-									, { class: classForDateBox(vnode, date) }
-									, m('.number', date)
-								)
-							})
-							, daysFromNextMonth(dateObj).map(function (date) {
-								return m('button.day.not-this-month', date)
-							})
-						)
+						, displayDate(State.date)
 					)
-					: null
+					, State.active
+						? State.yearView
+							? m(YearView, { viewObj: viewObj }) 
+							: m(MonthView, { viewObj: viewObj })
+						: null
+				)
+			)
+		}
+	}
+	
+	var Header = {
+		view: function (vnode) {
+			return m('header'
+				, m('button.prev'
+					, { onclick: vnode.attrs.stepFn.bind(null, -1) }
+				)
+				, m('button.month-year'
+					, { onclick: function(){ State.yearView = !State.yearView } }
+					, vnode.attrs.text
+				)
+				, m('button.next'
+					, { onclick: vnode.attrs.stepFn.bind(null, 1) }
+				)
+			)
+		}
+	}
+	
+	var YearView = fadeComponent()
+	YearView.view = function (vnode) {
+		return m('.calendar.incoming'
+			, m(Header, { stepFn: State.stepYear, text: vnode.attrs.viewObj.year })
+			, m('.months'
+				, months.map(function (month, idx) {
+					return m('button.month'
+						, {
+							class: classForMonthBox(idx),
+							onclick: State.jumpToMonth.bind(null, idx)
+						}
+						, m('.number', month.substring(0, 3))
+					)
+				})
 			)
 		)
 	}
-}
 
-if (typeof window.module === 'object') module.exports = DatePicker
-else window.DatePicker = DatePicker
+	var MonthView = fadeComponent()
+	MonthView.view = function (vnode) {
+		var viewObj = vnode.attrs.viewObj
+		return m('.calendar.incoming'
+			, m(Header, { stepFn: State.stepMonth, text: months[viewObj.month] + ' ' + viewObj.year })
+			, m('.weekdays'
+				, days.map(function (day) {
+					return m('.day.dummy', day.substring(0, 1))
+				})
+			)
+			, m('.weekdays'
+				, { onclick: State.chooseDate.bind(null, vnode) }
+				, daysFromLastMonth(viewObj).map(function (date) {
+					return m('button.day.not-this-month', date)
+				})
+				, daysFromThisMonth(viewObj).map(function (date) {
+					return m('button.day'
+						, { class: classForDateBox(vnode, date) }
+						, m('.number', date)
+					)
+				})
+				, daysFromNextMonth(viewObj).map(function (date) {
+					return m('button.day.not-this-month', date)
+				})
+			)
+		)
+	}
+
+	if (typeof window.module === 'object') module.exports = DatePicker
+	else window.DatePicker = DatePicker	
+})(window)
+
